c968350dec2af29bdd09b415231290a496b7f442
Adam Horak <adam@thehoraks.com>
2014-06-30 16:30:57 -0700

    changed jshint reporter

--- gulpfile.js
+++ gulpfile.js
@@ -534,7 +534,7 @@ gulp.task('lint', function() {
   return gulp.src(src)
     .pipe($.cached('linting'))
     .pipe($.jshint())
-    .pipe($.jshint.reporter('default'));
+    .pipe($.jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('clean', ['.clean:dev']);
