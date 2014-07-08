74117b1cdab387a089890e3e82018b147dd863eb
Adam Horak <adam@thehoraks.com>
2014-07-08 11:25:22 -0700

    removed debugging console.log"

--- app/scripts/components/map-display.js
+++ app/scripts/components/map-display.js
@@ -3,7 +3,6 @@
 App.MapDisplayComponent = Ember.Component.extend({
   didInsertElement: function() {
     this._super();
-    console.log('made it here');
     L.mapbox.map('map',
       Ember.config.MAPKEY)
       .setView(Ember.config.center, Ember.config.zoom);
