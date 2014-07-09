a2171e104151385b0c4f3075d414dc11e5da3e42
Adam Horak <adam@thehoraks.com>
2014-07-09 11:43:33 -0700

    removed completed todo comment

--- server/config/env/test.js
+++ server/config/env/test.js
@@ -8,6 +8,5 @@ module.exports = {
   public: path.resolve(path.join(__dirname, '../../../tmp/public')),
   db: {
     connection: 'mongodb://127.0.0.1:27017/test'
-    // TODO: Create a test db, populate w/ fixtures
   }
 };
