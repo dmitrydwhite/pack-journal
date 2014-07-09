fae52b2c12c11f6eb4d29b8643d7c5e7772d701a
Adam Horak <adam@thehoraks.com>
2014-07-09 09:53:56 -0700

    first (failing) test for trips api

--- test/server/test.js
+++ test/server/test.js
@@ -1,5 +1,27 @@
 'use strict';
 
-describe('server', function() {
-  it('will have tests');
+var expect = require('chai').expect;
+var request = require('request');
+var server = require('../../server/application');
+var helpers = require('../server_helper');
+
+describe('Trips API', function() {
+  before(function() {
+    server.listen(9000);
+    //helpers.setUpDBFixtures();
+    this.getTripsFixture = __fixture('get-trips');
+  });
+
+  it('Gets a valid list of all trips', function(done) {
+    request({
+      url: 'http://localhost:' + '9000' + this.getTripsFixture.request.url,
+      method: this.getTripsFixture.request.method
+    }, function(err, res, body) {
+      // TODO: Inspect res and make sure we get a 200 back
+      expect(body).to.eql(this.getTripsFixture.response);
+      console.log(body);
+      console.log(res);
+      done();
+    }.bind(this));
+  });
 });
