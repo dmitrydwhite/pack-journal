3fa9033a8332ed29a4fcd1c6fff52abceac05f25
Dmitry White <white.dmitry@gmail.com>
2014-07-11 11:46:13 -0700

    Updated changes

--- app/scripts/routes/trips-add-route.js
+++ app/scripts/routes/trips-add-route.js
@@ -15,6 +15,12 @@ App.TripsAddRoute = Ember.Route.extend({
   actions: {
     saveTrip: function() {
       console.log('Yep, save this trip');
+
+      var trip = this.store.createRecord('trip', {
+        name: this.get('controller.name')
+      });
+      console.log(trip);
+      // trip.save();   // Save this for later, serialization issues
       this.transitionTo('trip.edit', this);
     }
   }
