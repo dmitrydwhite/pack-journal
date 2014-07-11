60fa2b6a81eeef4f9db922ae25802eb585c426c2
Adam Horak <adam@thehoraks.com>
2014-07-11 15:31:13 -0700

    drops collection before setting up fixtures

--- test/server/helpers/fixture_helper.js
+++ test/server/helpers/fixture_helper.js
@@ -6,6 +6,7 @@ var Promise = require('bluebird');
 exports.setUpDBFixtures = function() {
   var trip = db.model('Trip');
   this.getTripsFixture = __fixture('get-trips');
+  trip.collection.drop();
   return Promise.all([
     trip.create(this.getTripsFixture.response.trips[0]),
     trip.create(this.getTripsFixture.response.trips[1])
