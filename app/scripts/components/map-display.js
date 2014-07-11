04344e9a802c94aa9c0767ec1ad5d2185b5e5e10
Adam Horak <adam@thehoraks.com>
2014-07-11 09:36:45 -0700

    refactored map display code

--- app/scripts/components/map-display.js
+++ app/scripts/components/map-display.js
@@ -1,41 +1,35 @@
 'use strict';
 
 App.MapDisplayComponent = Ember.Component.extend({
+  map: undefined,
+
+  routeLine: undefined,
+
   didInsertElement: function() {
     this._super();
+    this.set('map', L.mapbox.map('map', Ember.config.MAPKEY));
 
-    var linePoints = [];
-    var minLat = -90,
-        maxLat = 90,
-        minLong = -180,
-        maxLong = 180;
+    this.drawTrip();
+  },
+
+  drawTrip: function() {
+    var routePoints = [];
+    var defaultBounds = [[45.2, -122.9],[45.9,-122.3]];
 
     if(this.get('waypoints')) {
       this.get('waypoints').forEach(function(point) {
-        minLat  = point.latitude  > minLat  ? point.latitude  : minLat;
-        minLong = point.longitude > minLong  ? point.longitude : minLong;
-        maxLat  = point.latitude  < maxLat  ? point.latitude  : maxLat;
-        maxLong = point.longitude  < maxLong ? point.longitude  : maxLong;
-
-        linePoints.push([point.latitude, point.longitude]);
+        routePoints.push([point.latitude, point.longitude]);
       });
+      this.set('routeLine',
+        L.polyline(routePoints, { color: '#F99' }).addTo(this.get('map')));
+      this.get('map').fitBounds(this.get('routeLine').getBounds());
     } else {
-      minLat = 45.2;
-      minLong = -122.9;
-      maxLat = 45.9;
-      maxLong = -122.3;
+      this.get('map').fitBounds(defaultBounds);
     }
+  },
 
-    var map = L.mapbox.map('map', Ember.config.MAPKEY);
-      // .setView(Ember.config.center, Ember.config.zoom);
-
-    var southWest = L.latLng(minLat, minLong);
-    var northEast = L.latLng(maxLat, maxLong);
-    var bounds = L.latLngBounds(southWest, northEast);
-    map.fitBounds(bounds);
-
-    if(this.get('waypoints')) {
-      L.polyline(linePoints, { color: '#F99' }).addTo(map);
-    }
-  }
+  waypointsDidChange: function() {
+    this.get('map').removeLayer(this.get('routeLine'));
+    this.drawTrip();
+  }.observes('waypoints')
 });
