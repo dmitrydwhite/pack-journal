42a659a2c9a52b047e8fa3a8b7640c698a884405
Adam Horak <adam@thehoraks.com>
2014-07-11 14:31:16 -0700

    0.0.7

---
+++ dist/server/routes/trips.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var _ = require('lodash');
+
+var Trip = require('../models/trip').Trip;
+
+exports.getAll = function(req, res) {
+  Trip.find(function(err, docs) {
+    // TODO: Error handling generally
+    var mappedDoc = _.map(docs, function(doc) {
+      return {
+        id  : doc._id,
+        name: doc.name,
+        features: doc.features
+      };
+    });
+    res.json({ trips: mappedDoc });
+  });
+};
+
+exports.post = function(req, res) {
+  Trip.create(req.body.trip , function(err, doc) {
+    var mappedDoc = {
+      id: doc._id,
+      name: doc.name,
+      features: doc.features
+    };
+    res.json({ trip: mappedDoc });
+  });
+};
