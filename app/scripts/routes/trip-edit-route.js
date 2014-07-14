418d51a57b1999a99aabf666281971d836f53d9a
Adam Horak <adam@thehoraks.com>
2014-07-14 10:28:23 -0700

    working toward edit route

--- app/scripts/routes/trip-edit-route.js
+++ app/scripts/routes/trip-edit-route.js
@@ -11,5 +11,22 @@ App.TripEditRoute = Ember.Route.extend({
       outlet: 'sidebar',
       into: 'main'
     });
+  },
+
+  model: function(params) {
+    console.log(params);
+    return this.store.find('trip', params.id);
   }
+
+  // actions: {
+  //   saveTrip: function() {
+  //     var trip = this.store.createRecord('trip', {
+  //       name: this.get('controller.name')
+  //     });
+  //     trip.save()
+  //     .then(function(record) {
+  //       this.transitionTo('trip.edit', record);
+  //     }.bind(this));
+  //   }
+  // }
 });
