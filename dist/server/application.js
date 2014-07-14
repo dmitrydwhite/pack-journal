c0a405783e8cf8ad1b36c7b40d62b30138212648
Adam Horak <adam@thehoraks.com>
2014-07-14 14:53:44 -0700

    0.0.8

---
+++ dist/server/application.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var config = require('./config');
+if (config.env === 'production') { require('newrelic'); }
+
+var express = require('express');
+var path = require('path');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var compression = require('compression');
+var favicon = require('serve-favicon');
+var config = require('./config');
+
+var app = express();
+
+global.db = require('mongoose');
+db.connect(config.db.connection);
+var tripRoutes = require('./routes/trips');
+
+
+
+if (config.env === 'development') {
+  var connectLivereload = require('connect-livereload');
+  app.use(connectLivereload({ port: process.env.LIVERELOAD_PORT || 35729 }));
+  app.use(morgan('dev'));
+  app.use(express.static(config.public));
+  app.use(express.static(path.join(__dirname, '../app')));
+}
+if (config.env === 'production') {
+  app.use(morgan('default'));
+  app.use(favicon(path.join(config.public, 'favicon.ico')));
+  app.use(express.static(config.public));
+  app.use(compression());
+}
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+var api = express.Router();
+api.get('/trips', tripRoutes.getAll);
+api.get('/trips/:id', tripRoutes.get);
+api.post('/trips', tripRoutes.post);
+api.put('/trips/:id', tripRoutes.put);
+app.use('/api', api);
+
+// expose app
+module.exports = app;
+
+// start server
+if (require.main === module) {
+  app.listen(config.port, function() {
+    return console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
+  });
+}
---
+++ dist/server/routes/trips.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var _ = require('lodash');
+
+var Trip = require('../models/trip').Trip;
+
+exports.getAll = function(req, res) {
+  Trip.find(function(err, docs) {
+    // TODO: Error handling generally
+    var mappedDoc = _.map(docs, function(doc) {
+      return {
+        id  : doc._id,
+        name: doc.name,
+        features: doc.features
+      };
+    });
+    res.json({ trips: mappedDoc });
+  });
+};
+
+exports.get = function(req, res) {
+  Trip.findById(req.params.id, function(err, doc) {
+    res.json({
+      trip: {
+        id: doc._id,
+        name: doc.name,
+        features: doc.features
+      }
+    });
+  });
+};
+
+exports.post = function(req, res) {
+  Trip.create(req.body.trip , function(err, doc) {
+    var mappedDoc = {
+      id: doc._id,
+      name: doc.name,
+      features: doc.features
+    };
+    res.json({ trip: mappedDoc });
+  });
+};
+
+exports.put = function(req, res) {
+  Trip.findOneAndUpdate({ _id: req.params.id},
+    req.body.trip,
+    { new: true },
+    function(err, doc) {
+      var mappedDoc = {
+        id: doc._id,
+        name: doc.name,
+        features: doc.features
+    };
+    res.json({ trip: mappedDoc });
+  });
+};
