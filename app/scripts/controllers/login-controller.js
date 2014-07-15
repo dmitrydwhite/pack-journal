a8aaec4bf05f91b85d23acfb368df98d1abb7814
Adam Horak <adam@thehoraks.com>
2014-07-15 14:03:14 -0700

    displaying error div on login route

---
+++ app/scripts/controllers/login-controller.js
@@ -0,0 +1,23 @@
+App.LoginController = Ember.Controller.extend({
+  actions: {
+    authenticate: function() {
+      var self = this;
+      var session = this.get('session');
+      var credentials = this.getProperties('username', 'password');
+      this.set('error', undefined);
+      this.set('password', undefined);
+      session.authenticate(credentials).then(function() {
+        var attemptedTransition = self.get('attemptedTransition');
+        if (attemptedTransition) {
+          attemptedTransition.retry();
+          self.set('attemptedTransition', null);
+        } else {
+          self.transitionToRoute('trips');
+        }
+      })
+      .catch(function(error) {
+        self.set('error', error);
+      });
+    }
+  }
+});
