93747ca53ece5549199df05e444b30b130299d9d
Dmitry White <white.dmitry@gmail.com>
2014-07-15 16:02:36 -0700

    0.0.10

---
+++ dist/server/routes/users.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var User = require('../models/user').User;
+
+exports.post = function(req, res) {
+  res.json({
+    user: {
+      id: req.auth.user._id,
+      username: req.auth.user.username
+    }
+  });
+};
