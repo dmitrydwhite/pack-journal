0e88ef0cc5f31f95c281a51378494a516cf62654
Whitney Young <wbyoung@fadingred.com>
2014-07-17 13:37:19 -0700

    Fix rendering issue.

--- app/scripts/routes/trips-route.js
+++ app/scripts/routes/trips-route.js
@@ -1,12 +1,17 @@
 'use strict';
 
 App.TripsRoute = Ember.Route.extend({
+  renderTemplate: function() {
+    this.render('main');
+  }
+});
+
+App.TripsIndexRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('trip');
   },
 
   renderTemplate: function() {
-    this.render('main');
     this.render('map/default', {
       outlet: 'map',
       into: 'main'
