227f21b50aedeb8a74bf7dfe9b9c2bfd50972960
Dmitry White <white.dmitry@gmail.com>
2014-07-21 21:53:53 -0700

    Able to add multiple hikes and save.  Save button from edit route doesn't work.  Adding markers doesn't work.  Modified db schema definitions.

--- app/scripts/routes/trip-edit-route.js
+++ app/scripts/routes/trip-edit-route.js
@@ -2,10 +2,19 @@
 
 App.TripEditRoute = Ember.Route.extend({
   actions: {
-    saveTrip: function() {
+    saveTrip: function(params) {
       var self = this;
-      console.log(this.currentModel);
-      this.currentModel.save()
+      console.log(params);
+      var trip = {
+        id: this.get('controller.id'),
+        name: this.get('controller.name'),
+        waypoints: this.get('controller.waypoints'),
+        textAnnotations: this.get('controller.textAnnotations')
+      };
+      this.store.update('trip', trip)
+      .then(function(record) {
+        record.save();
+      })
       .then(function() {
         self.transitionTo('trips');
       })
