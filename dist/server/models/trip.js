df409a35050b2c414dfb485de7e29419103d45b8
Dmitry White <white.dmitry@gmail.com>
2014-07-22 14:59:15 -0700

    0.0.15

---
+++ dist/server/models/trip.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = (function() {
+  var Promise = require('bluebird');
+
+  // var CoordinateSchema = new db.Schema({
+  //   lat: Number,
+  //   lng: Number
+  // }, { id: false, _id: false });
+
+  // var TextAnnotationSchema = new db.Schema({
+  //   text: String,
+  //   coordinates: {
+  //     lat: Number,
+  //     lng: Number
+  //   }}, { id: false, _id: false });
+
+  var TripSchema = new db.Schema({
+    name: String,
+    owner: { type: db.Schema.Types.ObjectId, ref: 'User' },
+    features: {
+      // waypoints: [ CoordinateSchema ],
+      // textAnnotations: [ TextAnnotationSchema ]
+
+      waypoints:  Array ,
+      textAnnotations:   Array
+    }
+  });
+
+  var Trip = Promise.promisifyAll(db.model('Trip', TripSchema));
+  Promise.promisifyAll(Trip.prototype);
+
+  return { Trip: Trip };
+})();
