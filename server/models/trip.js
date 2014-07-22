13691ae0d6e807237e8ffaad8685e6b3ee3ac277
Dmitry White <white.dmitry@gmail.com>
2014-07-21 21:56:05 -0700

    Actual change from last commit message.

--- server/models/trip.js
+++ server/models/trip.js
@@ -3,24 +3,27 @@
 module.exports = (function() {
   var Promise = require('bluebird');
 
-  var CoordinateSchema = new db.Schema({
-    lat: Number,
-    lng: Number
-  }, { id: false, _id: false });
+  // var CoordinateSchema = new db.Schema({
+  //   lat: Number,
+  //   lng: Number
+  // }, { id: false, _id: false });
 
-  var TextAnnotationSchema = new db.Schema({
-    text: String,
-    coordinates: {
-      lat: Number,
-      lng: Number
-    }}, { id: false, _id: false });
+  // var TextAnnotationSchema = new db.Schema({
+  //   text: String,
+  //   coordinates: {
+  //     lat: Number,
+  //     lng: Number
+  //   }}, { id: false, _id: false });
 
   var TripSchema = new db.Schema({
     name: String,
     owner: { type: db.Schema.Types.ObjectId, ref: 'User' },
     features: {
-      waypoints: [ CoordinateSchema ],
-      textAnnotations: [ TextAnnotationSchema ]
+      // waypoints: [ CoordinateSchema ],
+      // textAnnotations: [ TextAnnotationSchema ]
+
+      waypoints: [ MixedSchema ],
+      textAnnotations: [ MixedSchema ]
     }
   });
 
