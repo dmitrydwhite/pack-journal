16e6df6c1380cfd67df0db305be0c6bc061688f2
Dmitry White <white.dmitry@gmail.com>
2014-08-01 14:33:16 -0700

    Corrected indenting for router.js file.

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -2,15 +2,15 @@
 
 Ember.Router.map(function () {
   this.resource('application', { path: '/'}, function() {
-  this.route('signup', { path: '/signup' });
-  this.route('login', { path: '/login' });
-  this.route('logout', { path: '/logout' });
-  this.route('trips', { path: '/trips' });
-  this.route('add', { path: '/add' });
-  this.resource('trip', { path: 'trips/:id' }, function() {
-    this.route('index', { path: '/' });
-    this.route('edit', { path: '/edit' });
-    this.route('delete', { path: '/delete' });
+    this.route('signup', { path: '/signup' });
+    this.route('login', { path: '/login' });
+    this.route('logout', { path: '/logout' });
+    this.route('trips', { path: '/trips' });
+    this.route('add', { path: '/add' });
+    this.resource('trip', { path: 'trips/:id' }, function() {
+      this.route('index', { path: '/' });
+      this.route('edit', { path: '/edit' });
+      this.route('delete', { path: '/delete' });
+      });
     });
-  });
 });
